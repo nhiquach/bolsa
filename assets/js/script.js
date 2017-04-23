7600f5496b7c2f515196a4cd10ded1f536c82be3
Nhi Quach <nhiquach@berkeley.edu>
2017-04-23 04:06:17 -0700

    captions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,16 @@ $(document).ready(function() {
 		$('html, body').animate({scrollTop: 1100}, 1000);
 	});
 
+	// Grid Puzzle 
+
 	$('#grid-0-0').click(function() {
 		if ( $('#0-0').css('display') == 'none' ){
 			$('#0-0').show()
+			$('#caption-0-0').show(); 
 			$('#0-0-back').hide()
 		} else {
 			$('#0-0').hide()
+			$('#caption-0-0').hide(); 
 			$('#0-0-back').show()
 		}
 	});
@@ -25,9 +29,11 @@ $(document).ready(function() {
 	$('#grid-0-1').click(function() {
 		if ( $('#0-1').css('display') == 'none' ){
 			$('#0-1').show()
+			$('#caption-0-1').show(); 
 			$('#0-1-back').hide()
 		} else {
 			$('#0-1').hide()
+			$('#caption-0-1').hide(); 
 			$('#0-1-back').show()
 		}
 	});
@@ -35,9 +41,11 @@ $(document).ready(function() {
 	$('#grid-0-2').click(function() {
 		if ( $('#0-2').css('display') == 'none' ){
 			$('#0-2').show()
+			$('#caption-0-2').show(); 
 			$('#0-2-back').hide()
 		} else {
 			$('#0-2').hide()
+			$('#caption-0-2').hide(); 
 			$('#0-2-back').show()
 		}
 	});
@@ -45,9 +53,11 @@ $(document).ready(function() {
 	$('#grid-1-0').click(function() {
 		if ( $('#1-0').css('display') == 'none' ){
 			$('#1-0').show()
+			$('#caption-1-0').show();
 			$('#1-0-back').hide()
 		} else {
 			$('#1-0').hide()
+			$('#caption-1-0').hide();
 			$('#1-0-back').show()
 		}
 	});
@@ -55,9 +65,11 @@ $(document).ready(function() {
 	$('#grid-1-1').click(function() {
 		if ( $('#1-1').css('display') == 'none' ){
 			$('#1-1').show()
+			$('#caption-1-1').show();
 			$('#1-1-back').hide()
 		} else {
 			$('#1-1').hide()
+			$('#caption-1-1').hide();
 			$('#1-1-back').show()
 		}
 	});
@@ -65,9 +77,11 @@ $(document).ready(function() {
 	$('#grid-1-2').click(function() {
 		if ( $('#1-2').css('display') == 'none' ){
 			$('#1-2').show()
+			$('#caption-1-2').show();
 			$('#1-2-back').hide()
 		} else {
 			$('#1-2').hide()
+			$('#caption-1-2').hide();
 			$('#1-2-back').show()
 		}
 	});
@@ -75,19 +89,23 @@ $(document).ready(function() {
 	$('#grid-2-0').click(function() {
 		if ( $('#2-0').css('display') == 'none' ){
 			$('#2-0').show()
+			$('#caption-2-0').show();
 			$('#2-0-back').hide()
 		} else {
 			$('#2-0').hide()
+			$('#caption-2-0').hide();
 			$('#2-0-back').show()
 		}
 	});
-	
+
 	$('#grid-2-1').click(function() {
 		if ( $('#2-1').css('display') == 'none' ){
 			$('#2-1').show()
+			$('#caption-2-1').show();
 			$('#2-1-back').hide()
 		} else {
 			$('#2-1').hide()
+			$('#caption-2-1').hide();
 			$('#2-1-back').show()
 		}
 	});
@@ -95,11 +113,105 @@ $(document).ready(function() {
 	$('#grid-2-2').click(function() {
 		if ( $('#2-2').css('display') == 'none' ){
 			$('#2-2').show()
+			$('#caption-2-2').show();
 			$('#2-2-back').hide()
 		} else {
 			$('#2-2').hide()
+			$('#caption-2-2').hide();
 			$('#2-2-back').show()
 		}
 	});
+
+	// Grid Captions
+	$(function() {
+			$('#grid-0-0').hover(function() { 
+				if ( $('#0-0').css('display') != 'none' ) {
+				    $('#caption-0-0').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-0-0').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-0-1').hover(function() { 
+				if ( $('#0-1').css('display') != 'none' ) {
+				    $('#caption-0-1').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-0-1').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-0-2').hover(function() { 
+				if ( $('#0-2').css('display') != 'none' ) {
+				    $('#caption-0-2').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-0-2').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-1-0').hover(function() { 
+				if ( $('#1-0').css('display') != 'none' ) {
+				    $('#caption-1-0').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-1-0').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-1-1').hover(function() { 
+				if ( $('#1-1').css('display') != 'none' ) {
+				    $('#caption-1-1').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-1-1').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-1-2').hover(function() { 
+				if ( $('#1-2').css('display') != 'none' ) {
+				    $('#caption-1-2').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-1-2').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-2-0').hover(function() { 
+				if ( $('#2-0').css('display') != 'none' ) {
+				    $('#caption-2-0').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-2-0').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-2-1').hover(function() { 
+				if ( $('#2-1').css('display') != 'none' ) {
+				    $('#caption-2-1').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-2-1').fadeOut(); 
+			});
+	});
+
+	$(function() {
+			$('#grid-2-2').hover(function() { 
+				if ( $('#2-2').css('display') != 'none' ) {
+				    $('#caption-2-2').fadeIn(); 
+				}
+			}, function() { 
+			    $('#caption-2-2').fadeOut(); 
+			});
+	});
+
 
 });
